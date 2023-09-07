0f80c2b0673142e4084416981d9d0b053e4fe11d
shazanasalehudin@gmail.com <shazanasalehudin@gmail.com>
2023-09-07 21:35:30 +0800

    Update GetConfig.cs
    update class

--- MVC_SYSTEM/Class/GetConfig.cs
+++ MVC_SYSTEM/Class/GetConfig.cs
@@ -464,20 +464,36 @@ namespace MVC_SYSTEM.Class
         }
 
         //Added by Shazana 1/8/2023
+        //Modified by Shazana 15/8/2023
         public string GetSyarikatFullName(string costcentre)
         {
             tblOptionConfigsWeb OptionConfigsWeb = new tblOptionConfigsWeb();
-
+            var SyarikatFullName = "";
             var syarikatInfo = db.tblOptionConfigsWebs.Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fldDeleted == false && x.fldOptConfValue == costcentre).FirstOrDefault();
-            var SyarikatFullName = db.tbl_Syarikat.Where(x => x.fld_SyarikatID == syarikatInfo.fld_SyarikatID && x.fld_NoSyarikat.Contains(syarikatInfo.fldOptConfDesc) && x.fld_NegaraID == syarikatInfo.fld_NegaraID).Select(s => s.fld_NamaSyarikat).FirstOrDefault();
-
+            if (syarikatInfo == null)
+            { SyarikatFullName = ""; }
+            else
+            {
+             var SyarikatFullNameInfo = db.tbl_Syarikat.Where(x => x.fld_SyarikatID == syarikatInfo.fld_SyarikatID && x.fld_NoSyarikat.Contains(syarikatInfo.fldOptConfDesc) && x.fld_NegaraID == syarikatInfo.fld_NegaraID).Select(s => s.fld_NamaSyarikat).FirstOrDefault();
+            if (SyarikatFullNameInfo != null)
+                {
+                    SyarikatFullName = SyarikatFullNameInfo;
+                }
+            }
             return SyarikatFullName;
         }
 
         //Added by Shazana 1/8/2023
+        //Modified by Shazana 15/8/2023
         public string GetSyarikatName(string costcentre)
         {
-            var SyarikatName = db.tblOptionConfigsWebs.Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fldDeleted == false && x.fldOptConfValue == costcentre).Select(s => s.fldOptConfDesc).FirstOrDefault();
+            var SyarikatName = "";
+            var SyarikatNameInfo = db.tblOptionConfigsWebs.Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fldDeleted == false && x.fldOptConfValue == costcentre).Select(s => s.fldOptConfDesc).FirstOrDefault();
+            if (SyarikatNameInfo == null)
+            { SyarikatName = ""; }
+            else
+            { SyarikatName = SyarikatNameInfo; }
+            
             return SyarikatName;
         }
     }
