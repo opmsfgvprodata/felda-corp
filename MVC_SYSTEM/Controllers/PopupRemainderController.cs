502169a0e9ad03e4f2c083c0943aa88d4673809f
sitimuzliana <sitimuzliana.95@gmail.com>
2023-09-08 12:05:23 +0800

    feature/optionfeldafpm(popupremainder)-yana

--- MVC_SYSTEM/Controllers/PopupRemainderController.cs
+++ MVC_SYSTEM/Controllers/PopupRemainderController.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC_SYSTEM.ModelsEstate;
+using System.Reflection.Emit;
 
 namespace MVC_SYSTEM.Controllers
 {
@@ -38,7 +39,7 @@ namespace MVC_SYSTEM.Controllers
         {
             int[] wlyhid = new int[] { };
             //string mywlyid = "";
-            int?  NegaraID = 0;
+            int? NegaraID = 0;
             int? SyarikatID = 0;
             int? WilayahID = 0;
             int? LadangID = 0;
@@ -46,26 +47,65 @@ namespace MVC_SYSTEM.Controllers
 
             GetNSWL.GetData(out NegaraID, out SyarikatID, out WilayahID, out LadangID, getuserid, User.Identity.Name);
 
+            //yana add 030823
+            List<SelectListItem> SyarikatList = new List<SelectListItem>();
+            SyarikatList = new SelectList(
+                db.tblOptionConfigsWebs
+                    .Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fld_NegaraID == NegaraID &&
+                                x.fld_SyarikatID == SyarikatID && x.fldDeleted == false)
+                    .OrderBy(o => o.fldOptConfDesc)
+                    .Select(s => new SelectListItem { Value = s.fldOptConfValue, Text = s.fldOptConfDesc }),
+                "Value", "Text").ToList();
+            // end here 030823
+
             List<SelectListItem> WilayahIDList = new List<SelectListItem>();
 
             if (WilayahID == 0 && LadangID == 0)
             {
+                // yana add 060923
+                SyarikatList = new SelectList(
+                db.tblOptionConfigsWebs
+                    .Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fld_NegaraID == NegaraID &&
+                                x.fld_SyarikatID == SyarikatID && x.fldDeleted == false)
+                    .OrderBy(o => o.fldOptConfDesc)
+                    .Select(s => new SelectListItem { Value = s.fldOptConfValue, Text = s.fldOptConfDesc }),
+                "Value", "Text").ToList();
+                // end here 060923
                 wlyhid = getwilyah.GetWilayahID(SyarikatID);
                 WilayahIDList = new SelectList(db2.tbl_Wilayah.Where(x => wlyhid.Contains(x.fld_ID)), "fld_ID", "fld_WlyhName").ToList();
             }
             else if (WilayahID != 0 && LadangID == 0)
             {
+                // yana add 060923
+                SyarikatList = new SelectList(
+                db.tblOptionConfigsWebs
+                    .Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fld_NegaraID == NegaraID &&
+                                x.fld_SyarikatID == SyarikatID && x.fldDeleted == false)
+                    .OrderBy(o => o.fldOptConfDesc)
+                    .Select(s => new SelectListItem { Value = s.fldOptConfValue, Text = s.fldOptConfDesc }),
+                "Value", "Text").ToList();
                 wlyhid = getwilyah.GetWilayahID2(SyarikatID, WilayahID);
                 WilayahIDList = new SelectList(db2.tbl_Wilayah.Where(x => wlyhid.Contains(x.fld_ID)), "fld_ID", "fld_WlyhName").ToList();
             }
             else if (WilayahID != 0 && LadangID != 0)
             {
+                // yana add 060923
+                SyarikatList = new SelectList(
+                db.tblOptionConfigsWebs
+                    .Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fld_NegaraID == NegaraID &&
+                                x.fld_SyarikatID == SyarikatID && x.fldDeleted == false)
+                    .OrderBy(o => o.fldOptConfDesc)
+                    .Select(s => new SelectListItem { Value = s.fldOptConfValue, Text = s.fldOptConfDesc }),
+                "Value", "Text").ToList();
                 wlyhid = getwilyah.GetWilayahID2(SyarikatID, WilayahID);
                 WilayahIDList = new SelectList(db2.tbl_Wilayah.Where(x => wlyhid.Contains(x.fld_ID)), "fld_ID", "fld_WlyhName").ToList();
             }
 
-            var getPermitLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, wlyhid.Take(1).FirstOrDefault(), LadangID, "1").Where(x => x.fld_BilBlnTmtPrmt <= 3 && x.fld_KdRyt != "MA").Select(s => s.fld_LadangID).Distinct().ToList();
-            var getPassportLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, wlyhid.Take(1).FirstOrDefault(), LadangID, "1").Where(x => x.fld_BilBlnTmtPsprt <= 3 && x.fld_KdRyt != "MA").Select(s => s.fld_LadangID).Distinct().ToList();
+            // yana add 060923
+            var compCode = db.tbl_Ladang.Where(x => x.fld_ID == LadangID).Select(s => s.fld_CostCentre).FirstOrDefault();
+            // yana add compcode
+            var getPermitLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, wlyhid.Take(1).FirstOrDefault(), LadangID, "1", compCode).Where(x => x.fld_BilBlnTmtPrmt <= 3 && x.fld_KdRyt != "MA").Select(s => s.fld_LadangID).Distinct().ToList();
+            var getPassportLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, wlyhid.Take(1).FirstOrDefault(), LadangID, "1", compCode).Where(x => x.fld_BilBlnTmtPsprt <= 3 && x.fld_KdRyt != "MA").Select(s => s.fld_LadangID).Distinct().ToList();
             var getTaskRemainder = dbC.tblTaskRemainders.Where(x => x.fldWilayahID == wlyhid.Take(1).FirstOrDefault() && x.fldStatus == 0).Select(s => new { s.fldLadangID, s.fldPurpose }).ToList();
             var getNewUsrIDAppLadangID = getTaskRemainder.Where(x => x.fldPurpose == "02").Select(s => s.fldLadangID).Distinct().ToList();
             var getSlryIncAppLadangID = getTaskRemainder.Where(x => x.fldPurpose == "03").Select(s => s.fldLadangID).Distinct().ToList();
@@ -86,13 +126,15 @@ namespace MVC_SYSTEM.Controllers
             //ViewBag.getnewworkerappwilayahID = getnewworkerappwilayahID;
             //ViewBag.getnewuseridappwilayahID = getnewuseridappwilayahID;
             //ViewBag.ladangvalue = LadangID;
+            ViewBag.SyarikatList = SyarikatList;
             ViewBag.WilayahIDList = WilayahIDList;
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index(int WilayahIDList)
+        // yana add 070923 - string SyarikatList
+        public ActionResult Index(int WilayahIDList, string SyarikatList)
         {
             int[] wlyhid = new int[] { };
             //string mywlyid = "";
@@ -104,6 +146,19 @@ namespace MVC_SYSTEM.Controllers
 
             GetNSWL.GetData(out NegaraID, out SyarikatID, out WilayahID, out LadangID, getuserid, User.Identity.Name);
 
+            //yana add 030823
+            List<SelectListItem> SyarikatList2 = new List<SelectListItem>();
+            SyarikatList2 = new SelectList(
+                db.tblOptionConfigsWebs
+                    .Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fld_NegaraID == NegaraID &&
+                                x.fld_SyarikatID == SyarikatID && x.fldDeleted == false)
+                    .OrderBy(o => o.fldOptConfDesc)
+                    .Select(s => new SelectListItem { Value = s.fldOptConfValue, Text = s.fldOptConfDesc }),
+                "Value", "Text").ToList();
+
+            ViewBag.SyarikatList = SyarikatList2;
+
+            // end here 030823
             List<SelectListItem> WilayahIDList2 = new List<SelectListItem>();
 
             if (WilayahID == 0 && LadangID == 0)
@@ -124,9 +179,10 @@ namespace MVC_SYSTEM.Controllers
                 wlyhid = getwilyah.GetWilayahID2(SyarikatID, WilayahID);
                 WilayahIDList2 = new SelectList(db2.tbl_Wilayah.Where(x => wlyhid.Contains(x.fld_ID)), "fld_ID", "fld_WlyhName", WilayahIDList).ToList();
             }
-
-            var getPermitLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, WilayahIDList, LadangID, "1").Where(x => x.fld_BilBlnTmtPrmt <= 3 && x.fld_KdRyt != "MA").Select(s => s.fld_LadangID).Distinct().ToList();
-            var getPassportLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, WilayahIDList, LadangID, "1").Where(x => x.fld_BilBlnTmtPsprt <= 3 && x.fld_KdRyt != "MA").Select(s => s.fld_LadangID).Distinct().ToList();
+            //var costcentercode = db.tblOptionConfigsWebs.Where(x => x.fldOptConfFlag1 == "kodSAPSyarikat" && x.fldOptConfDesc == costcentre).FirstOrDefault();
+            // yana update 070923 - add SyarikatList
+            var getPermitLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, WilayahIDList, LadangID, "1", SyarikatList).Where(x => x.fld_BilBlnTmtPrmt <= 3 && x.fld_KdRyt != "MA" && x.fld_CostCentre == SyarikatList).Select(s => s.fld_LadangID).Distinct().ToList();
+            var getPassportLadangID = sp_db.sp_PermitPassportDetail(NegaraID, SyarikatID, WilayahIDList, LadangID, "1", SyarikatList).Where(x => x.fld_BilBlnTmtPsprt <= 3 && x.fld_KdRyt != "MA" && x.fld_CostCentre == SyarikatList).Select(s => s.fld_LadangID).Distinct().ToList();
             var getTaskRemainder = dbC.tblTaskRemainders.Where(x => x.fldWilayahID == WilayahIDList && x.fldStatus == 0).Select(s => new { s.fldLadangID, s.fldPurpose }).ToList();
             var getNewUsrIDAppLadangID = getTaskRemainder.Where(x => x.fldPurpose == "02").Select(s => s.fldLadangID).Distinct().ToList();
             var getSlryIncAppLadangID = getTaskRemainder.Where(x => x.fldPurpose == "03").Select(s => s.fldLadangID).Distinct().ToList();
@@ -152,23 +208,23 @@ namespace MVC_SYSTEM.Controllers
             ViewBag.WilayahIDList = WilayahIDList2;
             return View();
         }
-
-        public ActionResult LaporanTamatPermit(int LadangID)
+        // yana update 070923 - add string SyarikatList
+        public ActionResult LaporanTamatPermit(int LadangID, string SyarikatList)
         {
             var NSWL = GetNSWL.GetLadangDetail(LadangID);
-
-            var LaporanTamatPermit = sp_db.sp_PermitPassportDetail(NSWL.fld_NegaraID, NSWL.fld_SyarikatID, NSWL.fld_WilayahID, NSWL.fld_LadangID, "1").Where(x => x.fld_BilBlnTmtPrmt <= 3 && x.fld_KdRyt != "MA").OrderBy(o=>o.fld_BilBlnTmtPrmt).ToList();
+            // yana update 070923 - add SyarikatList
+            var LaporanTamatPermit = sp_db.sp_PermitPassportDetail(NSWL.fld_NegaraID, NSWL.fld_SyarikatID, NSWL.fld_WilayahID, NSWL.fld_LadangID, "1", SyarikatList).Where(x => x.fld_BilBlnTmtPrmt <= 3 && x.fld_KdRyt != "MA").OrderBy(o => o.fld_BilBlnTmtPrmt).ToList();
             ViewBag.WilayahName = NSWL.fld_NamaWilayah;
             ViewBag.LadangName = NSWL.fld_NamaLadang;
             ViewBag.TotalCount = LaporanTamatPermit.Count();
             return View(LaporanTamatPermit);
         }
-
-        public ActionResult LaporanTamatPassport(int LadangID)
+        // yana update 070923 - add string SyarikatList
+        public ActionResult LaporanTamatPassport(int LadangID, string SyarikatList)
         {
             var NSWL = GetNSWL.GetLadangDetail(LadangID);
-
-            var LaporanTamatPassport = sp_db.sp_PermitPassportDetail(NSWL.fld_NegaraID, NSWL.fld_SyarikatID, NSWL.fld_WilayahID, NSWL.fld_LadangID, "1").Where(x => x.fld_BilBlnTmtPsprt <= 3 && x.fld_KdRyt != "MA").OrderBy(o => o.fld_BilBlnTmtPsprt).ToList();
+            // yana update 070923 - add SyarikatList
+            var LaporanTamatPassport = sp_db.sp_PermitPassportDetail(NSWL.fld_NegaraID, NSWL.fld_SyarikatID, NSWL.fld_WilayahID, NSWL.fld_LadangID, "1", SyarikatList).Where(x => x.fld_BilBlnTmtPsprt <= 3 && x.fld_KdRyt != "MA").OrderBy(o => o.fld_BilBlnTmtPsprt).ToList();
             ViewBag.WilayahName = NSWL.fld_NamaWilayah;
             ViewBag.LadangName = NSWL.fld_NamaLadang;
             ViewBag.TotalCount = LaporanTamatPassport.Count();
@@ -220,23 +276,31 @@ namespace MVC_SYSTEM.Controllers
             return View(TaskNewWorkerApp);
         }
 
-        public ActionResult TaskNewUserIDApp(int LadangID)
+        //  yana add - string SyarikatList
+        public ActionResult TaskNewUserIDApp(int LadangID, string SyarikatList)
         {
             int? getuserid = getidentity.ID(User.Identity.Name);
 
             var NSWL = GetNSWL.GetLadangDetail(LadangID);
 
-            var checknotascfiles = dbC.tblASCApprovalFileDetails.Where(x => x.fldASCFileStatus == 1 && x.fldPurpose == 1 && x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID &&  x.fldGenStatus == 0).Select(s => s.fldFileName).ToList();
-           
-            var TaskNewUserIDApp = dbC.tblTaskRemainders.Where(x => x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID && x.fldStatus == 0 && x.fldPurpose == "02" && checknotascfiles.Contains(x.fldFileName)).OrderBy(o => o.fldDateTimeStamp).ToList();
+            //var compCode = db.tbl_Ladang.Where(x => x.fld_ID == LadangID).Select(s => s.fld_CostCentre).FirstOrDefault();
+
+            //var ladangid = db.tbl_Ladang.Where(x => x.fld_ID == LadangID && x.fld_CostCentre == compCode).FirstOrDefault();
+
+            var checknotascfiles = dbC.tblASCApprovalFileDetails.Where(x => x.fldASCFileStatus == 1 && x.fldPurpose == 1 && x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID && x.fldGenStatus == 0).Select(s => s.fldFileName).ToList();
+
+            // yana add - NSWL.fld_CostCentre == SyarikatList
+            var TaskNewUserIDApp = dbC.tblTaskRemainders.Where(x => x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID && x.fldStatus == 0 && x.fldPurpose == "02" && checknotascfiles.Contains(x.fldFileName) && NSWL.fld_CostCentre == SyarikatList).OrderBy(o => o.fldDateTimeStamp).ToList();
             ViewBag.WilayahName = NSWL.fld_NamaWilayah;
             ViewBag.LadangName = NSWL.fld_NamaLadang;
+            //ViewBag.compCode = NSWL.fld_CostCentre;
             ViewBag.TotalCount = TaskNewUserIDApp.Count();
 
             return View(TaskNewUserIDApp);
         }
 
-        public ActionResult TaskSalaryIncrmntApp(int LadangID)
+        //  yana add - string SyarikatList
+        public ActionResult TaskSalaryIncrmntApp(int LadangID, string SyarikatList)
         {
             int? getuserid = getidentity.ID(User.Identity.Name);
 
@@ -244,7 +308,8 @@ namespace MVC_SYSTEM.Controllers
 
             var checknotascfiles = dbC.tblASCApprovalFileDetails.Join(dbC.tblTaskRemainders, j => new { j.fldFileName, j.fldNegaraID, j.fldSyarikatID, j.fldWilayahID, j.fldLadangID }, k => new { k.fldFileName, k.fldNegaraID, k.fldSyarikatID, k.fldWilayahID, k.fldLadangID }, (j, k) => new { j.fldASCFileStatus, j.fldPurpose, j.fldGenStatus, j.fldNegaraID, j.fldSyarikatID, j.fldWilayahID, j.fldLadangID, j.fldFileName }).Where(x => x.fldASCFileStatus == 1 && x.fldPurpose == 3 && x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID && x.fldGenStatus == 0).Select(s => s.fldFileName).ToList();
 
-            var TaskSalaryIncrmntApp = dbC.tblTaskRemainders.Where(x => x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID && x.fldStatus == 0 && x.fldPurpose == "03" && checknotascfiles.Contains(x.fldFileName)).OrderBy(o => o.fldDateTimeStamp).ToList();
+            // yana add - NSWL.fld_CostCentre == SyarikatList
+            var TaskSalaryIncrmntApp = dbC.tblTaskRemainders.Where(x => x.fldNegaraID == NSWL.fld_NegaraID && x.fldSyarikatID == NSWL.fld_SyarikatID && x.fldWilayahID == NSWL.fld_WilayahID && x.fldLadangID == NSWL.fld_LadangID && x.fldStatus == 0 && x.fldPurpose == "03" && checknotascfiles.Contains(x.fldFileName) && NSWL.fld_CostCentre == SyarikatList).OrderBy(o => o.fldDateTimeStamp).ToList();
             ViewBag.WilayahName = NSWL.fld_NamaWilayah;
             ViewBag.LadangName = NSWL.fld_NamaLadang;
             ViewBag.TotalCount = TaskSalaryIncrmntApp.Count();
