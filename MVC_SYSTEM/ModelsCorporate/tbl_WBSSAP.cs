7ad4eddaf4e1b585713eb31b905ac864663789dc
shazanasalehudin@gmail.com <shazanasalehudin@gmail.com>
2023-12-29 15:29:49 +0800

    SAP Master Data
    SAP MAster Data

---
+++ MVC_SYSTEM/ModelsCorporate/tbl_WBSSAP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SYSTEM.ModelsCorporate
+{
+    public class tbl_WBSSAP
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int fld_ID { get; set; }
+       public string fld_wbsElement { get; set; }
+        public string fld_wbsDescription { get; set; }
+        public string fld_createdby { get; set; }
+        public DateTime? fld_createdDate { get; set; }
+        public string fld_updatedby { get; set; }
+        public DateTime? fld_updatedDate { get; set; }
+        public bool? fld_Deleted { get; set; }
+
+    }
+
+    [Table("tbl_WBSSAP")]
+    public class tbl_WBSSAPCreate
+    {
+
+        public string fld_CompanyCode { get; set; }
+
+        [Required(ErrorMessage = "WBS Code is required.")]
+        [Display(Name = "WBS Element")]
+        public string fld_wbsElement { get; set; }
+       
+    }
+
+   
+}
